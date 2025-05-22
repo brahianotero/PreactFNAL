e267863579d3fe5865fd3b676818f3ac3286c4b9
rodrigo <rodrigobetancourth1999@gmail.com>
2025-05-21 21:47:29 -0500

    Subi  carpetas de lo que vamos a manejar  vacías

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-/*import "./styles.css";
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ClienteForm from "./components/ClienteForm";
-import ListaProductos from "./components/ListaProductos";
-import Carrito from "./components/Carrito";
-import { CompraProvider } from "./context/CompraContext";
-
-function App() {
-  return (
-    <CompraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<ClienteForm />} />
-          <Route path="/productos" element={<ListaProductos />} />
-          <Route path="/carrito" element={<Carrito />} />
-        </Routes>
-      </Router>
-    </CompraProvider>
-  );
-}
-
-export default App;*/
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
